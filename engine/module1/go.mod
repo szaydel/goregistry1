2e04f8f1b4943f5fad74a90c67fcd4700b476005
Sam Zaydel <szaydel@gmail.com>
2020-03-01 17:02:42 +0000

    WIP

--- engine/module1/go.mod
+++ engine/module1/go.mod
@@ -1,3 +1,5 @@
 module github.com/szaydel/goregistry1/engine/module1
 
 go 1.13
+
+require github.com/szaydel/goregistry1/engine/registry v0.0.0-20200301170645-ccbc64de92cd
--- engine/registry/registry.go
+++ engine/registry/registry.go
@@ -13,6 +13,8 @@ type Registry struct {
 	Modules map[string]Module
 }
 
+var Default = Registry{}
+
 func (r *Registry) Register(m Module) {
 	fmt.Printf("Registering %s\n", m.Identity())
 	r.Modules[m.Identity()] = m
