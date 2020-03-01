5a7086d11277f5a2dee4ba81c12482f01ea373b2
Sam Zaydel <szaydel@gmail.com>
2020-03-01 15:16:10 +0000

    Added registry

---
+++ engine/registry/go.mod
@@ -0,0 +1,3 @@
+module github.com/szaydel/goregistry1/engine/registry
+
+go 1.13
---
+++ engine/registry/registry.go
@@ -0,0 +1,19 @@
+package registry
+
+import (
+	"fmt"
+
+	// "github.com/szaydel/goregistry1/engine/module1"
+)
+type Module interface {
+	Identity() string
+}
+
+type Registry struct {
+	Modules map[string]Module
+}
+
+func (r *Registry) Register(m Module) {
+	fmt.Printf("Registering %s\n", m.Identity())
+	r.Modules[m.Identity()] = m
+}
